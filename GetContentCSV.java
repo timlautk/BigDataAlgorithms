f52a76aeb58affa5ce70e1c59b7c7e778c3d81a3
Tim Lau <timlautk@gmail.com>
2017-05-25 01:39:51 +0200

    Update GetContentCSV.java

--- GetContentCSV.java
+++ GetContentCSV.java
@@ -10,15 +10,15 @@ public class GetContentCSV {
 	  FSDataInputStream inStream = fs.open(filename);
     try {
     	InputStreamReader isr = new InputStreamReader(inStream);
-		BufferedReader br = new BufferedReader(isr);
+	BufferedReader br = new BufferedReader(isr);
 		
-		for (String line = br.readLine(); line != null; line = br.readLine()) {
+	for (String line = br.readLine(); line != null; line = br.readLine()) {
             String[] data = line.split(";");
             System.out.println(data[5] + "\t" + data[6]);
         }
     } finally {
     	inStream.close();
-		fs.close();
+	fs.close();
     }
   }
 }
